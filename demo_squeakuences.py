03cceed9187907ee8266a72732c69bb2d8ea1d00
Linnea Lane <linnealane@gmail.com>
2024-03-30 16:08:05 -0700

    Add original and modified id names to a dictionary as key-value pair and write names to rows in tsv

--- demo_squeakuences.py
+++ demo_squeakuences.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 import argparse
+import csv
 
 def removeSpaces(seqName):
   modifiedName = seqName.strip()
   modifiedName = modifiedName.replace(' ', '')
   modifiedName = modifiedName.replace('\t', '')
   return modifiedName
+
+def writeModIDFile(idDictInput):
+  with open('ids.csv', 'w') as tsvfile:
+    writer = csv.writer(tsvfile, delimiter='\t')
+    for k, v in idDictInput.items():
+      writer.writerow([k, v])
+  tsvfile.close()
 
 #Set up an argumanet parser
 parser = argparse.ArgumentParser(description='Quick and Dirty Squeakuences Model')
@@ -21,13 +29,18 @@ fasta=args.file
 fasta_handle = open(fasta, 'r')
 
 count = 0
+idDict = {}
 
 for line in fasta_handle:
   if line.startswith('>'):
     count += 1
     #print(line)
-    id = line.strip('>')
-    id = id.strip('\n')
-    print(removeSpaces(id))
+    line = line.strip('>')
+    line = line.strip('\n')
+    id = removeSpaces(line)
+    
+    idDict.update({line: id})
 
+print(idDict)
+writeModIDFile(idDict)
 print(count)
