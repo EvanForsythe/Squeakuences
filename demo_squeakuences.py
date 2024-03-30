b50f5c4a41c911c0cb2c6aeba63533b45077de0c
Linnea Lane <linnealane@gmail.com>
2024-03-30 15:10:45 -0700

    Set up terminal interface with argparse and print each > line to terminal with > total count

--- demo_squeakuences.py
+++ demo_squeakuences.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+import argparse
+
+#Set up an argumanet parser
+parser = argparse.ArgumentParser(description='Quick and Dirty Squeakuences Model')
+
+parser.add_argument('-f', '--file', type=str, metavar='', required=True, help='Full path to fasta file to clean') 
+
+#Define the parser
+args = parser.parse_args()
+
+#Store arguments
+fasta=args.file
+
+fasta_handle = open(fasta, 'r')
+
+count = 0
+
+for line in fasta_handle:
+  if line.startswith('>'):
+    count += 1
+    print(line)
+
+print(count)
