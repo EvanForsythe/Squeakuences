2367d885a87f7fc66307e820a3e9fe111d948457
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-03-30 00:06:20 -0700

    Create tests for some basic functions for squeakuences and create those functions in the squeakuences.py file

--- squeakuences.py
+++ squeakuences.py
@@ -2,3 +2,23 @@
 
 def hello_world():
   return "Hello world!"
+
+def load_file(sequence_id):
+  return
+
+def is_squence_id(sequence_id):
+  return
+
+#######################################
+# Non Alpha-Numeric Removal Functions #
+#######################################
+
+def remove_brackets(sequence_id):
+  return
+
+def remove_punctuation(sequence_id):
+  return
+
+# Optional?
+def remove_non_english_characters(sequence_id):
+  return
