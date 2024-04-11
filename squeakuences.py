35b89c2033ebe9992b2fc8aaaf185dbf58236e11
Ashley Doerfler <doerflas@oregonstate.edu>
2024-04-11 13:01:50 -0700

    Add comments about regexes to squeakuences.py

--- squeakuences.py
+++ squeakuences.py
@@ -17,14 +17,17 @@ def is_squence_id(line):
 
 def remove_brackets(sequence_id):
   # TODO: Remove any brackets from the sequence ID
+  # Multiple regexes: /\(.*\)/, /\[.*\]/, /\{.*\}/, /\-.*/
   return
 
 def remove_punctuation(sequence_id):
   # TODO: Remove any punctuation from the sequence ID.
   # Be aware of the first >. How are we going to approach this?
+  # regex: /<[\`\=\+\:\_\.\\\"\?\¿\!\¡\.\;\:\&\$\*\@\%\#]>/
   return
 
 # Optional?
 def remove_non_english_characters(sequence_id):
   # TODO: Remove any non-english characters
+  # Regex: /<-[a..zA..Z\s]>+/  <<< Do we need this to include numbers? This is saying anything that isn't an english alphabetic letter
   return
