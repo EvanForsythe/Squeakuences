7cbd963dd1df5a8cc54981bb204fa451bd3fcbbf
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-04-16 15:00:54 -0700

    fix tests

--- test_squeakuences.py
+++ test_squeakuences.py
@@ -6,21 +6,21 @@ class TestStringMethods(unittest.TestCase):
 
   # Does load_file load file properly
   def testLoadFile(self):
-    faFile, fastaHandle = squeakuences.loadFile(test.txt)
+    faFile, fastaHandle = squeakuences.loadFile('test.txt')
     self.assertEqual(faFile, 'test')
     self.assertInstance(fastHandle, _io.TextIOWrappe)
 
   # Does is_squence_id identify a line begining with > as being true
   def testIsSequenceIdTrue(self):
-    self.assertEqual(squeakuences.isSequenceId('> This line is true'), true)
+    self.assertEqual(squeakuences.isSequenceId('> This line is true'), True)
 
   # Does is_squence_id identify a line not begining with > as being false
   def testIsSequenceIdFalse(self):
-    self.assertEqual(squeakuences.isSequenceId('This line is false'), false)
+    self.assertEqual(squeakuences.isSequenceId('This line is false'), False)
 
   # Does remove_brackets remove brackets from a given line
   def test_remove_brackets(self):
-    self.assertEqual(squeakuences.removeNonAlphanumeric('Re(move{ br[ack}]ets)'), 'Remove brackets')
+    self.assertEqual(squeakuences.removeNonAlphanumeric('Re(mov{e br[ack}]ets)'), 'Remove brackets')
 
   # Does remove_punctuation remove all puntuation from a given line
   def test_remove_punctuation(self):
