ae6d751a5cdd480117759fb59432b726c617223e
Ashley Doerfler <doerflas@oregonstate.edu>
2024-04-16 14:45:09 -0700

    Update test_squeakuences.py

--- test_squeakuences.py
+++ test_squeakuences.py
@@ -20,15 +20,15 @@ class TestStringMethods(unittest.TestCase):
 
   # Does remove_brackets remove brackets from a given line
   def test_remove_brackets(self):
-    self.assertEqual(squeakuences.remove_brackets('Re(move{ br[ack}]ets)'), 'Remove brackets')
+    self.assertEqual(squeakuences.removeNonAlphanumeric('Re(move{ br[ack}]ets)'), 'Remove brackets')
 
   # Does remove_punctuation remove all puntuation from a given line
   def test_remove_punctuation(self):
-    self.assertEqual(squeakuences.remove_punctuation('.R`e=m+o:v_e. \p"u?n¿c!¡t:;u&a$t*i@*o%#n'), 'Remove punctuation')
+    self.assertEqual(squeakuences.removeNonAlphanumeric('.R`e=m+o:v_e. \p"u?n¿c!¡t:;u&a$t*i@*o%#n'), 'Remove punctuation')
 
   # Does remove_non_english_characters remove any non english characters
   def test_remove_non_english_characters(self):
-    self.assertEqual(squeakuences.remove_non_english_characters('R¥emÙove ÅnoĦn-engŧlish chaŸracters'), 'Remove non-english characters')
+    self.assertEqual(squeakuences.removeNonAlphanumeric('R¥emÙove ÅnoĦn-engŧlish chaŸracters'), 'Remove non-english characters')
 
 if __name__ == '__main__':
   unittest.main()
