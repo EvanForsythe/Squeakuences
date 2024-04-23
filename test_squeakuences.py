1165c4bdb77e3e9347cc0e3dc9fb9ced1cb9c8df
Linnea Lane <linnealane@gmail.com>
2024-04-23 13:25:13 -0700

    add/edit comments for tests

--- test_squeakuences.py
+++ test_squeakuences.py
@@ -16,6 +16,7 @@ from pyfakefs import fake_filesystem_unittest
 # TODO: add test for writing to file?
 
 class TestFileMethods(fake_filesystem_unittest.TestCase):
+  # Set up the fake file system
   def setUp(self):
     self.setUpPyfakefs()
     if not os.path.exists('test.fa'):
@@ -30,6 +31,7 @@ class TestFileMethods(fake_filesystem_unittest.TestCase):
     self.assertEqual(faFileName, 'test')
     fastaHandle.close()
 
+  # Does checkExisting remove file(s) from previous squeakuences runs
   def test_checkExisting(self):
     self.setUp()
     filePathTrue = 'test.fa'
@@ -56,39 +58,42 @@ class TestFileMethods(fake_filesystem_unittest.TestCase):
 
 
 class TestSequenceMethods(unittest.TestCase):
-  # Does is_squence_id identify a line begining with > as being true
+  # Does isSequenceId identify a line begining with > as being true
   def test_isSequenceIdTrue(self):
     self.assertEqual(squeakuences.isSequenceId('> This line is true'), True)
 
-  # Does is_squence_id identify a line not begining with > as being false
+  # Does isSequenceId identify a line not begining with > as being false
   def test_isSequenceIdFalse(self):
     self.assertEqual(squeakuences.isSequenceId('This line is false'), False)
 
+  # Does stripSequenceId remove > and \n from a given line
   def test_stripSequenceId(self):
     funcInput = '>Galgal_14-3-3 protein gamma\n'
     funcOutput = 'Galgal_14-3-3 protein gamma'
     self.assertEqual(squeakuences.stripSequenceId(funcInput), funcOutput)
 
-  # Does remove_brackets remove brackets from a given line
+  # Does removeNonAlphanumeric remove brackets from a given line
   def test_removeNonAlphanumeric(self):
     self.assertEqual(squeakuences.removeNonAlphanumeric('Re(mov{e br[ack}]ets)'), 'Remove brackets')
     self.assertEqual(squeakuences.removeNonAlphanumeric('.R`e=m+o:v_e. /p"u?n¿c!¡t:;u&a$t*i@*o%#n'), 'Remove punctuation')
     self.assertEqual(squeakuences.removeNonAlphanumeric('Acachl_[Pyruvate dehydrogenase [acetyl-transferring]]-phosphatase 2, mitochondrial'), 'AcachlPyruvate dehydrogenase acetyltransferringphosphatase 2 mitochondrial')
 
-  # Does remove_non_english_characters remove any non english characters
+  # Does removeNonAlphanumeric remove any non english characters
   def test_remove_non_english_characters(self):
     self.assertEqual(squeakuences.removeNonAlphanumeric('R¥emÙove ÅnoĦn-engŧlish chaŸracters'), 'Remove nonenglish characters')
 
-  # Does remove_non_english_characters remove any non english characters
+  # Does removeSpaces remove whitespace
   def test_removeSpaces(self):
     self.assertEqual(squeakuences.removeSpaces('Remove Spaces '), 'RemoveSpaces')
     self.assertEqual(squeakuences.removeSpaces('Remove  Spaces'), 'RemoveSpaces')
     self.assertEqual(squeakuences.removeSpaces('Acachl_aminoacyl tRNA synthase complex-interacting multifunctional protein 1-like, partial'), 'Acachl_aminoacyltRNAsynthasecomplex-interactingmultifunctionalprotein1-like,partial')
 
+  # Does speciesName ensure that the sequence id always begins with the species name and an underscore
   def test_speciesName(self):
     self.assertEqual(squeakuences.speciesName('AcachlArg8VasotocinReceptorLike', 'Acachl'), 'Acachl_Arg8VasotocinReceptorLike')
     self.assertEqual(squeakuences.speciesName('Arg8VasotocinReceptorLike', 'Acachl'), 'Acachl_Arg8VasotocinReceptorLike')
 
+  # Does chop shorten a sequence id to either the default or given maximum length
   def test_chop(self):
     self.assertEqual(squeakuences.chop('Acachl_PyruvateDehydrogenaseAcetylTransferringKinaseIsozyme1Mitochondrial'), 'Acachl_PyruvateDehydrogenaseAcetyl___KinaseIsozyme1Mitochondrial')
     self.assertEqual(squeakuences.chop('Acachl_ADisintegrinAndMetalloproteinaseWithThrombospondinMotifs18Partial'), 'Acachl_ADisintegrinAnd___WithThrombospondinMotifs18Partial')
