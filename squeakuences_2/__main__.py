68a4d0a3805d17b27c8e0cd4000798100059a4f8
Linnea Lane <linnealane@gmail.com>
2024-10-15 22:20:06 -0700

    implement error checking for empty squeakify list

--- squeakuences_2/__main__.py
+++ squeakuences_2/__main__.py
@@ -16,9 +16,17 @@ inputType = cli.resolveInputType(argsDict['input'])
 
 if inputType == 'non-path':
   print('You\'ve passed a non-path string into the input flag. Please review your input and try again.')
+  print('Exiting Squeakuences run now.')
   sys.exit()
   
 print('You\'ve input a ' + inputType + '.')
 
 squeakifyList = file_system.compileSqueakifyList(inputType, argsDict)
+
+if squeakifyList == []:
+  print('--------------------------------')
+  print('Squeakuences did not find any files with the ' + str(argsDict['fileExt']) + ' extension at the input directory location.')
+  print('Please check your command and try again.')
+  print('Exiting Squeakuences run now.')
+  sys.exit()
 
