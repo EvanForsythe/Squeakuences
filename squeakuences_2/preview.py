3e7ddb2e69a1b29d9b64de5a121fb89a75fbd9c3
Linnea Lane <linnealane@gmail.com>
2024-10-17 22:49:11 -0700

    spit out all cleaned ids to terminal from one file

--- squeakuences_2/preview.py
+++ squeakuences_2/preview.py
@@ -1,2 +1,17 @@
-def generatePreview():
+import file_system
+import squeakify
+
+def generatePreview(argsDict):
   print('Preview mode activated!')
+  sequenceIdCount = 0
+
+  messyFastaHandle = file_system.loadMessyFile(argsDict['input'])
+
+  for line in messyFastaHandle:
+    if line.startswith('>'):
+      sequenceIdCount += 1
+      sequenceID = squeakify.stripSequenceID(line)
+
+      cleanSequenceId = squeakify.squeakify(sequenceID, argsDict)
+
+      print(cleanSequenceId)
